add53cb7457ee15489007499f8b1d46c8c50234c
sample <sample@naver.com>
2023-02-20 11:54:02 +0900

    rename

--- z-project/src/main/java/com/example/project/GetheaderApplication.java
+++ z-project/src/main/java/com/example/project/GetheaderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZProjectApplication {
+public class GetheaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZProjectApplication.class, args);
+		SpringApplication.run(GetheaderApplication.class, args);
 	}
 
 }
